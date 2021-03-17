d8bfd76a9543dcb9415e1797ae9bd151d88a1783
Bhubanesh Mishra <bhubanesh440mishra@gmail.com>
2021-03-17 17:16:18 +0000

    Final implementation Code

---
+++ Final/basic/Worker.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 12 12:05:30 2021
+
+@author: mininet
+"""
+from Blockchain import Blockchain
+from Block import Block
+from datetime import datetime
+from flask import Flask,jsonify,abort,request
+import json
+import threading
+import time
+import requests
+import sys
+""
+from uuid import uuid4
+class Worker:
+    def __init__(self, host, port, debug, IPs):
+        # host: the Worker ip
+        # port: the Worker port
+        # debug: the Worker debug mode
+        # IPs: the Miners' IP addresses
+        self.host = host
+        self.port = port
+        self.debug = False
+        self.IPs = IPs
+        # blockchain: Worker type blockchain
+        self.blockchain = Blockchain('Worker')
+        # Flask app creation of worker
+        self.app = Flask(__name__)
+        # Flask method1: View the blockchain on Worker by index.
+        @self.app.route('/blockchains/<int:id>',methods=['GET'])
+        def get_blockchains(id):
+            data = {
+                    'index': self.blockchain.blocks[id].index,
+                    'timestamp': self.blockchain.blocks[id].timestamp,
+                    'transaction': self.blockchain.blocks[id].transaction,
+                    'previous_hash': self.blockchain.blocks[id].previous_hash,
+                    'mac': self.blockchain.blocks[id].mac,
+                    'hash': self.blockchain.blocks[id].hash
+                    }
+            return jsonify(data)
+        # Flask app method2: Receiver of Miner, receive block from miners
+        # After receiving block, it processes the block directly.
+        @self.app.route('/workerReceiver', methods=['POST'])
+        def receive_block():
+            if not request.data:
+                abort(400)
+            str = request.data.decode('utf-8')
+            str_json = json.loads(str)
+            # Process the block
+            self.process(str_json)
+            return jsonify(str_json)
+        # Start worker directly.
+        self.start()
+        
+    # start contains main thread and a sub-thread.
+    
+    def start(self):
+        # Start listen thread
+        subthread = threading.Thread(target = self.listen)
+        subthread.start()
+        
+        # Set a timeout for getting Genesis block
+        time.sleep(12)
+        # Generate blocks periodly on main thread
+        while True:
+            print("Worker while loop generate block.")
+            # Generate and block to Miner.
+            new_block = self.generate_block()
+            data = {
+                    'timestamp': new_block.timestamp,
+                    'transaction': new_block.transaction,
+                    'mac': new_block.mac
+                    }
+            # Send the block to all Miners.
+            # in our following extension, may multiple Miners.
+            for i in range(len(self.IPs)):
+                url = 'http://' + self.IPs[i] + ':5000/minerReceiver'
+                response = requests.post(url , data = json.dumps(data))
+                if i == 0:
+                    print("New Generated Common Block content:")
+                    print(response.content)
+
+            time.sleep(15)
+            
+            
+    def listen(self):
+        print("Start sub-thread for listening. (Worker)")
+        self.app.run(self.host,
+                     self.port,
+                     self.debug,
+                     threaded = True)
+    # Generate a new block.
+    def generate_block(self):
+        index = len(self.blockchain.blocks)
+        """index,"""
+        # When a new block is generated, the basic info sent is
+        # timestamp, transaction, and uuid of this worker.
+        """ index, previous hash and hash are used after verification."""
+        new_block = Block(index,
+                          str(datetime.now()),
+                          'Block_transaction',
+                          self.blockchain.blocks[index-1].hash,
+                          'Worker_MAC(uuid)'
+                     )
+        return new_block
+            
+    def process(self, str_json):
+        "index, timestamp, transaction, previous_hash, mac"
+        time = str_json['timestamp']
+        transaction = str_json['transaction']
+        mac = str_json['mac']
+        
+        if len(str_json) == 5:
+            preHash = str_json['previous_hash']
+            "can add a verify and return a result"
+            # verify_block(?) 
+            print("This Worker receives first block.")
+            self.blockchain.add_blockG(time, transaction, preHash, mac)
+        else:
+            print("This Worker receives common block.")
+            "can add a verify and return a result"
+            # verify_block(?)
+            self.blockchain.add_block(time, transaction, mac)
+    
+    "Thomas public key decyption"
+    def verify_block(self, block):
+        pass
+    
+if __name__ == '__main__':
+    # Test mode
+    """In basic version, just one Miner, in our first and second extension,
+       more Miners are used, so that we can use an IP list as input.
+    """
+   # w = Worker('10.0.0.2', '5000', False, ['10.0.0.1'])
+    
+    # Combination mode
+    "When we combine Starter.py, it can set the parameter."
+    #print(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4:])
+    w = Worker(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4:])
+    
+    
+    
---
+++ Final/extension1/correct/Miner.py
@@ -0,0 +1,318 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 13 10:57:10 2021
+
+@author: mininet
+"""
+from flask import Flask,jsonify,abort,request
+import json
+import threading
+import requests
+import time
+from Blockchain import Blockchain
+import sys
+"uuid can be used in extension 1. MAC address"
+from uuid import uuid4
+from WorkerData import WorkerData
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
+from Crypto.PublicKey import RSA
+
+class Miner:
+    def __init__(self, host, port, debug, minersSize,worker_nodes, minerIPs):
+        # host: the host ip of Miner
+        # port: port of Miner connection
+        # debug: on/off debug mode
+        # IPs: the ip addresses of workers
+        self.host = host
+        self.port = port
+        self.debug = False
+        self.minersSize = minersSize
+        self.worker_nodes = worker_nodes
+        # blockchains: the blockchain of Miner. its type is Miner, 
+        # which contains Gensesis Block.
+        self.blockchains = Blockchain('Miner')
+        # Waiting list of miner, all received blocks are stored in it in order.
+        self.waitingList = []
+        # use it to compare results from other miners
+        self.compareList = []
+        
+        self.minerIPList = minerIPs
+        # Flask app creation
+        self.app = Flask(__name__)
+        # Flask app method1: View the blockchain on miner by index.
+        @self.app.route('/blockchains/<int:id>',methods=['GET'])
+        def get_blockchains(id):
+            data = {
+                    'index': self.blockchains.blocks[id].index,
+                    'timestamp': self.blockchains.blocks[id].timestamp,
+                    'transaction': self.blockchains.blocks[id].transaction,
+                    'previous_hash': self.blockchains.blocks[id].previous_hash,
+                    'mac': self.blockchains.blocks[id].mac,
+                    'hash': self.blockchains.blocks[id].hash
+                    }
+            return jsonify(data)
+           
+        # Flask app method2: Receiver of Miner, receive block from workers
+        # After receiving block, it adds the block to Waiting list directly.
+        @self.app.route('/minerReceiver', methods=['POST'])
+        def receive_block():
+            print("Receive block from: " + request.remote_addr)
+            if not request.data:
+                abort(400)
+            str = request.data.decode('utf-8')
+            str_json = json.loads(str)
+            self.waitingList.append(str_json)
+            return jsonify(str_json)
+        
+        #route that stores the public key of the workers
+        @self.app.route('/key-receiver', methods=['POST'])
+        def receive_public_key():
+            ip_addr = request.remote_addr
+            print("received key from:" + ip_addr)
+            json_data = request.get_json()
+            pub_key = RSA.import_key(json_data['pub_key'].encode('utf-8'))
+            worker = self.get_worker(ip_addr) 
+            if(worker == None):
+                print("worker with IP " + ip_addr + " not found") 
+                return jsonify({"error": "Worker not stored on the miner"}), 401
+            else:
+                index_of_worker = worker_nodes.index(worker)
+                worker_nodes[index_of_worker].pub_key = pub_key
+                return jsonify({"msg": "Key is updated"}), 201
+        # ##################################
+        @self.app.route('/verify-receiver', methods=['POST'])
+        def verify_miner():
+            # other miner ip address
+            ip_addr = request.remote_addr
+            print("received block from Miner:" + ip_addr)
+            #json_data = request.get_json()
+            str = request.data.decode('utf-8')
+            str_json = json.loads(str)
+            data = {
+                'ip': ip_addr, 
+                'timestamp': str_json['timestamp'],
+                'transaction': str_json['transaction'],
+                'mac': str_json['mac']
+                }
+            self.compareList.append(data)
+            return jsonify(str_json)
+        # Directly start the Miner.
+        #
+        print(self.minersSize)
+        print(self.minerIPList)
+        self.minerIPList.remove(str(self.host))
+        """for i in range(int(self.minersSize)):
+            str = self.minerIPList[i]
+            if str == self.host:
+                self."""
+        self.start()
+        
+    # start contains the main thread and a sub-thread. 
+    # Main thread: always checking waiting list
+    def start(self):
+        
+        # 1. Start sub-thread to listen workers for public key and block.
+        # Firstly, listen to all workers for their publick key.
+        subthread1 = threading.Thread(target = self.listen)
+        subthread1.start()
+        
+        time.sleep(5)
+        # 2. Send the Gensesis Block to all workers.
+        self.sendGenBlock()
+        
+        time.sleep(3)
+        # 3. Main thread always check waiting list.
+        while True:
+            print("Main thread is checking waitling list.")
+            if len(self.waitingList) > 0:
+                print("Waiting list length > 0, Miner verifies a block.")
+                self.processBlock()
+            time.sleep(15)
+            
+    # Send the genesis block created by Miner to all workers.
+    def sendGenBlock(self):
+        "Gensesis Block content"
+        data = {
+                'index': self.blockchains.blocks[0].index,
+                'timestamp': self.blockchains.blocks[0].timestamp,
+                'transaction': self.blockchains.blocks[0].transaction,
+                'previous_hash': self.blockchains.blocks[0].previous_hash,
+                'mac': self.blockchains.blocks[0].mac
+                }
+        #  Send to all workers
+        for i in range(len(self.worker_nodes)):
+            url = 'http://' + self.worker_nodes[i].ip + ':5000/workerReceiver'
+            response = requests.post(url , data = json.dumps(data))
+            if i == 0:
+                print("Created Gensesis Block content:")
+                print(response.content)   
+                
+    # Flask app start
+    def listen(self):
+        print("Start sub-thread for listening.")
+        self.app.run(self.host,
+                     self.port,
+                     self.debug,
+                     threaded = True)
+        
+    # Process block
+    def processBlock(self):
+        print("Process one block:")
+        signed_Block = self.waitingList[0]
+        "verify may contain decyptions, drop block and others?"
+        
+        processedBlock = self.verify_block(signed_Block)
+        self.broadcastMiners(processedBlock)
+        
+        if(processedBlock != None):
+            ## add a check at there
+            while True:
+                time.sleep(1)
+                print("Checking compareList for miners:")
+                if len(self.compareList) == int(self.minersSize):
+                
+                    processedBlock = self.verify_miners(processedBlock)
+                    self.blockchains.add_block(processedBlock['timestamp'], 
+                                        processedBlock['transaction'],
+                                        processedBlock['mac'])
+                    self.broadcast(processedBlock)
+                    break
+                
+    def verify_miners(self, processedBlock):
+
+        l = []
+        for i in self.compareList: 
+            compare_format = {
+                    'timestamp': i['timestamp'],
+                    'transaction': i['transaction'],
+                    'mac': i['mac']
+                    }
+            l.append(compare_format)
+       
+        maxResult = (max(l, key = l.count))
+        print("maxxxxxxxxxxx" + str(maxResult))
+        for i in range(len(l)):
+            if l[i] == maxResult:
+                print("Miner on" + str(self.compareList[i]['ip']) + " is right.")
+            else:
+                print("Miner on" + str(self.compareList[i]['ip']) + " is corrupt.")
+                print("Its content: " + str(l[i]))
+        self.compareList.clear()
+        return maxResult
+    # Validates a block
+    def verify_block(self, signed_block):
+        del(self.waitingList[0]) #block is removed from the list in any case
+        
+        str_block = signed_block['block']
+        json_block = json.loads(str_block.replace("'",'"'))
+        mac = json_block['mac'] 
+        signature = signed_block['signature']
+        hash = SHA256.new(str_block.encode())
+        pub_key = self.get_key(mac)
+        "the pub_key needs to be compared using string, otherwise crash"
+        if(str(pub_key) == "None"):
+             print('No public key for given MAC address')
+             return False
+
+        try:
+            pkcs1_15.new(pub_key).verify(hash,bytearray.fromhex(signature))
+                    #value = 0
+            itself = {
+                    'ip': self.host, 
+                    'timestamp': json_block['timestamp'],
+                    'transaction': json_block['transaction'],
+                    'mac': json_block['mac']                
+                    }
+            self.compareList.append(itself)
+            print('block was successfully validated')
+            #################
+            """
+            self.blockchains.add_block(json_block['timestamp'], 
+                                        json_block['transaction'],
+                                        json_block['mac'])
+            self.broadcastMiners(json_block)
+            """
+            return json_block
+        except(ValueError,TypeError): 
+            print('block is not valid!')
+            return None
+
+    # Broadcast common block to all workers.
+    def broadcast(self, block):
+        print("Broadcast the block")
+        print(block)
+        data = {
+                # 'index': len(self.blockchains.blocks), deleted
+                'timestamp': block['timestamp'],
+                'transaction': block['transaction'],
+                # 'previous_hash': self.blockchains.blocks[len(self.blockchains.blocks) - 1].previous_hash, deleted
+                'mac': block['mac']
+                }
+        for i in range(len(self.worker_nodes)):
+            url = 'http://' + self.worker_nodes[i].ip + ':5000/workerReceiver'
+            response = requests.post(url , data = json.dumps(data))
+            if i == 0:
+                print("Broadcast Common Block content:")
+                print(response.content)
+    def broadcastMiners(self, block):
+        print("Broadcast the block to other miners")
+        
+        data = {
+                'timestamp': block['timestamp'],
+                'transaction': block['transaction'],
+                'mac': block['mac']
+                }
+        
+        ipList = self.minerIPList
+        
+     
+        print(ipList)
+        for ip in ipList:
+            url = 'http://' + ip + ':5000/verify-receiver'
+            response = requests.post(url , data = json.dumps(data))
+        print(response)
+    #helper function
+    def get_key(self,mac):
+        for worker in self.worker_nodes:
+            if(worker.mac ==  mac):
+                return worker.pub_key
+        return None
+
+    #helper function
+    def get_worker(self,ip_addr):
+        for worker in self.worker_nodes:
+            if(worker.ip ==  ip_addr):
+                return worker
+        return None  
+
+def createWorker(WorkerDataList, minerSize):
+     print(sys.argv[5:-minerSize])
+     for i in (sys.argv[5:-minerSize]):
+        list = i.split('.')
+        print(list)
+       
+        macStr = '00:11:22:33:44:' + list[3]
+        
+        print(macStr)
+        WorkD = WorkerData(i, macStr)
+        WorkerDataList.append(WorkD)
+        
+def minerList(minerSize):
+    minerList = []
+    for i in range(1, int(minerSize) + 1):
+        ip = '10.0.0.'+ str(i)
+        minerList.append(ip)
+    
+    return minerList
+        
+if __name__ == '__main__': 
+    # Test mode
+    # Miner's ip is 10.0.0.1
+    # All workers' port are 5000
+    minerIPList = minerList(sys.argv[4])
+    WorkerDataList =[]
+    createWorker(WorkerDataList, len(minerIPList))
+    print(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], WorkerDataList)
+    miner = Miner(sys.argv[1], sys.argv[2], False, sys.argv[4], WorkerDataList, minerIPList)
